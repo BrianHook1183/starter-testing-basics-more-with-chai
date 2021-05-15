8f320c61b3f7be7d00e26d2929ccdc9aef88e45c
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-05-15 02:13:27 -0500

    notes and example code added

--- tests/main.test.js
+++ tests/main.test.js
@@ -13,7 +13,11 @@ describe("medianScore", () => {
     const actual = medianScore(students);
     const expected = 8.4;
     expect(actual).to.be.a("number");
-    expect(actual).to.equal(expected);
+  });
+  it("returns null if no array given", () => {
+    const students = [];
+    const actual = medianScore(students);
+    expect(actual).to.be.null;
   });
 });
 
@@ -29,6 +33,11 @@ describe("topScoringStudent", () => {
     const actual = topScoringStudent(students);
     const expected = { name: "Riley Morgan", score: 9.8 };
     expect(actual).to.be.an("object");
-    expect(actual).to.equal(expected);
+    expect(actual).to.eql(expected);
+  });
+  it("returns null if no array given", () => {
+    const students = [];
+    const actual = topScoringStudent(students);
+    expect(actual).to.be.null;
   });
 });
